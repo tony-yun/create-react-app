9f747dc21a61321c9b36ba541b4671332f8dd872
윤태웅 <yuntaewoong@yuntaeung-ui-MacBookPro.local>
2022-08-06 15:34:32 +0900

    changeofstyledcompo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Father>
+      <Box bgColor="tomato" />
+      <Circle bgColor="teal" />
+    </Father>
   );
-}
+};
 
 export default App;
+
+const Father = styled.div`
+  display: flex;
+  flex: 1;
+`;
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
+  width: 200px;
+  height: 200px;
+`;
+const Circle = styled(Box)`
+  border-radius: 100px;
+`;
